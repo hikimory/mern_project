0bebebfd7675bf74d4f259ab8db2e24622cc0353
Nick Bykon <nikita.bikon@gmail.com>
2020-09-01 18:43:36 +0300

    add request validation

--- backend/routes/admin/auth.js
+++ backend/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const {Router} = require('express')
 const auth = require('../../controllers/admin/auth')
+const { validateSignUpRequest, isRequestValidated, validateSignInRequest } = require('../../validators/auth')
 
 const route = Router()
 
-route.post('/admin/signup', auth.signUp)
-route.post('/admin/signin', auth.signIn)
+route.post('/admin/signup', [validateSignUpRequest, isRequestValidated], auth.signUp)
+route.post('/admin/signin', [validateSignInRequest, isRequestValidated], auth.signIn)
 
 module.exports = route
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const {Router} = require('express')
+const {check} = require('express-validator')
 const auth = require('../controllers/auth')
 const auth_mdw = require('../middleware/auth.middleware')
+const { validateSignUpRequest, isRequestValidated, validateSignInRequest } = require('../validators/auth')
 
 const route = Router()
 
-route.post('/signup', auth.signUp)
-route.post('/signin', auth.signIn)
+route.post('/signup', [validateSignUpRequest, isRequestValidated], auth.signUp)
+route.post('/signin', [validateSignInRequest, isRequestValidated], auth.signIn)
 
 route.post('/profile', auth_mdw, (req, res) => {
     res.status(200).json({ user: 'profile' })
